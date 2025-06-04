acfcf5b0ea7c37712fbc13931c387c280a762ad0
a-jednak <zjedynak@student.agh.edu.pl>
2025-06-04 19:39:57 +0200

    Spring boot initialized

---
+++ climbinggym/src/main/java/com/project/climbinggym/ClimbinggymApplication.java
@@ -0,0 +1,13 @@
+package com.project.climbinggym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbinggymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbinggymApplication.class, args);
+	}
+
+}
---
+++ climbinggym/src/test/java/com/project/climbinggym/ClimbinggymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.climbinggym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbinggymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
